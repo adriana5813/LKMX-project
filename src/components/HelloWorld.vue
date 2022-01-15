511e3980e91f9ef5312cf2ad3b65c7abbecfd50e
adriana5813 <adriana.bg.15@outlook.es>
2022-01-14 20:36:45 -0800

    ToDoList

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,97 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+  <center><div class="wrapper">
+    <h1>To Do List</h1>
+    <form>
+      <input
+        type="text"
+        name="todo-text"
+        v-model="newTodoText"
+        placeholder="Escribe una tarea"       
+      />
+      <button @click.prevent="addTodo">Agregar</button>
+    </form>
+    <ul v-if="todos.length">
+      <TodoItem
+        v-for="todo in todos"
+        :key="todo.id"
+        :todo="todo"
+        @remove="removeTodo"
+        @edit="editTodo"
+      />
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <p class="none" v-else>No has agregado tareas.</p>
+  </div></center>
 </template>
 
 <script>
+import TodoItem from "./TodoItem.vue";
+
+let nextTodoId = 1;
+
+const createTodo = (text) => ({
+  text,
+  id: nextTodoId++,
+});
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  components: {
+    TodoItem,
+  },
+
+  data() {
+    return {
+      todos: [],
+
+      newTodoText: "",
+    };
+  },
+
+  methods: {
+    addTodo() {
+      const trimmedText = this.newTodoText.trim();
+
+      if (trimmedText) {
+        this.todos.push(createTodo(trimmedText));
+      }
+
+      this.newTodoText = "";
+    },
+
+    removeTodo(item) {
+      this.todos = this.todos.filter((todo) => todo !== item);
+    },
+    editTodo(item) {
+      var objIndex = this.todos.findIndex((obj) => obj.id == item.id);
+      this.todos[objIndex].text = "";
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style >
+.wrapper {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 10px;
+  width: 40%;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+h1{
+    text-align: left;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+input{
+    width: 80%;
+    text-align: left;
 }
-a {
-  color: #42b983;
+button{
+    background-color: cornflowerblue;
+    border: none;
+    margin-left:5px;
+    font-size: 15px;
+    border-radius: 3px;
+    color: white;
+}
+form{
+    width: 100%;
 }
 </style>
+
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <center><li>
+    <input v-model="todo.text" placeholder="Tarea" class="input"> 
+    <button @click.prevent="$emit('remove', todo)" id="b1" class="buton">Eliminar</button>
+    <button @click.prevent="$emit('edit', todo)" id="b2" class="buton">Editar</button>
+  </li></center>
+</template>
+
+
+<script>
+export default {
+  props: {
+    todo: {
+      required: true,
+      type: Object
+    }
+  }
+}
+</script>
+
+<style >
+#b1 {background-color: #f44336;} 
+#b2 {background-color: #4CAF50;}
+.buton{
+    color: white;
+    border: none;
+    margin-left:5px;
+    font-size: 15px;
+    border-radius: 3px;
+    
+}
+li{
+    margin-top: 5px;
+    width: 100%;
+    list-style-type: none;
+}
+input{
+    width: 60%;
+    text-align: left;
+}
+
+
+</style>
+
+
